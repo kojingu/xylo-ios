ee4f55f514c8e613570fe1b537a8f3e37cbba3cd
Nolberto Castellanos Rodriguez <nolberto.castellanos@amcnetworks.com>
2022-12-24 10:46:24 -0500

    Added the view model for the create game screen

---
+++ xylo/UI/Screens/CreateGame/CreateGameViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CreateGameViewModel.swift
+//  xylo
+//
+//  Created by Nolberto Castellanos Rodriguez on 24/12/22.
+//
+
+import Foundation
+
+extension CreateGameScreen {
+    
+    class ViewModel: ObservableObject {
+        
+    }
+}
